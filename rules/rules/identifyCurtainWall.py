a55c6057f7d108cae9a293aac6e802bedda1e378
Raghadhamza <s235564@dtu.dk>
2024-11-04 15:10:17 +0100

    Delete rules/rules directory

--- rules/rules/identifyCurtainWall.py
+++
@@ -1,6 +0,0 @@
-import ifcopenshell
-
-def identify_curtain_walls(model):
-    curtain_walls = model.by_type('IfcCurtainWall')
-    result = f"Curtain Walls: {len(curtain_walls)}"
-    return curtain_walls, result
