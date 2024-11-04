ba1b685d127775bde14c5cacac8965e72fe8b862
Raghadhamza <s235564@dtu.dk>
2024-11-04 15:10:03 +0100

    Delete rules/rules/rules directory

--- rules/rules/rules/analyzeCurtainWallWindows.py
+++
@@ -1,6 +0,0 @@
-def analyze_curtain_wall_windows(curtain_walls):
-    windows = []
-    for wall in curtain_walls:
-        windows.extend(wall.IsDefinedBy)  # Adjust based on actual attributes
-    result = f"Windows in Curtain Walls: {len(windows)}"
-    return windows, result
